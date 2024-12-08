cbbea824a5f4febcc5b50a0462928150e4ae5d4b
NaufalParamaRafif <profesoraiueo@gmail.com>
2024-12-08 21:30:44 +0700

    Add search (just display)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-// Toggle class active
-const navbarNav = document.querySelector('.navbar-nav');
-document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
+// Toggle class active for hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
-const hamburger = document.querySelector('#hamburger-menu');
-document.addEventListener('click', function(e) {
-  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-    navbarNav.classList.remove('active');
+// Toggle class active for search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const searchButton = document.querySelector("#search-button");
+
+searchButton.onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
